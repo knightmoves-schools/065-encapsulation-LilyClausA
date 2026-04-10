9557b2b0dad4174ea92b64ae05062c3eeeca63cb
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-04-10 17:29:39 +0000

    fbgyhuj

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ let height = 10;
 function calculateArea(length, height){
     return length * height;
 }
+
+//should encapsulate the variables 
+// and functions within a Rectangle
+
+//should pass the variables into the
+//  Rectangle as constructor arguments
+
+//should have a calculateArea method 
+// that takes no arguments
